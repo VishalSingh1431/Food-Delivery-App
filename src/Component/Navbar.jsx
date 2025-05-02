4bc85e3b23e5be8af792dd2b8aac0abb7b9957a5
Vishahl99 <vishalkumar.singh.cer21@itbhu.ac.in>
2025-05-02 18:33:07 +0530

    .

--- src/Component/Navbar.jsx
+++ src/Component/Navbar.jsx
@@ -5,7 +5,7 @@ const Navbar = () => {
     <div className='bg-rgb(188, 204, 220) h-20 flex justify-between p-5'>
 {/* shadow-[0px_-10px_20px_rgba(0,255,255,0.3),0px_10px_20px_rgba(255,255,255,0.3)] shadow-white rounded-2xl */}
       <div>
-        <h1 className='shadow-[0px_-10px_20px_rgba(0,255,255,0.3),0px_10px_20px_rgba(255,255,255,0.3)] shadow-white rounded-2xl text-2xl p-0.5'>FOOD</h1>
+        <h1 className='shadow-[0px_-10px_20px_rgba(0,255,255,0.3),0px_10px_20px_rgba(255,255,255,0.3)] shadow-white rounded-2xl text-2xl p-0.5'>VIPRA FOOD</h1>
       </div>
       <div>
         <ul className='flex gap-3 '>
